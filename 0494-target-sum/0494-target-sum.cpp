927095cb09559843ed53d65c3167ba97976ef7c0
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2025-07-17 09:35:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        if(nums.size()<target) return 0;
+        int n = nums.size();
+        int mod = 1e9 + 7;
+        int sum = accumulate(nums.begin(),nums.end(),0);
+        target = (target + sum)/2;
+        vector<vector<int>> dp(n + 1, vector<int>(target + 1, 0));
+        
+
+        for (int i = 0; i <= n; i++){
+            dp[i][0] = 1;
+        }
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= target; j++) {
+                if (nums[i - 1] <= j)
+                    dp[i][j] = (dp[i - 1][j] + dp[i - 1][j - nums[i - 1]]) % mod;
+                else    
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+        
+        return dp[n][target];
+    }
+};
