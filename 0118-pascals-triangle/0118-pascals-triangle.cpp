5f2023976811ebf80ea1de1b1235ebe44d5a2d3c
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2025-08-01 23:00:11 +0530

    Time: 1 ms (19.21%), Space: 9.8 MB (30.63%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -4,20 +4,19 @@ public:
         vector<vector<int>> dp;
 
         dp.push_back({1});
-        int i=0;
+        int i = 0;
         n--;
-        while(n--){
+        while (n--) {
             vector<int> v;
-        for(int j=0;j<dp[i].size()+1;j++){
-            if(j==0 || j==dp[i].size()){
-                v.push_back(1);
+            for (int j = 0; j < dp[i].size() + 1; j++) {
+                if (j == 0 || j == dp[i].size()) {
+                    v.push_back(1);
+                } else {
+                    v.push_back(dp[i][j] + dp[i][j - 1]);
+                }
             }
-            else{
-                v.push_back(dp[i][j] + dp[i][j-1]);
-            }
-        }
-        dp.push_back(v);
-        i++;
+            dp.push_back(v);
+            i++;
         }
 
         return dp;
