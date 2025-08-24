f659bb8c505cca511b2f08c45950cedc91eee229
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2025-08-24 15:35:12 +0530

    Time: 3 ms (28.78%), Space: 16.8 MB (75.24%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,63 +1,50 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        queue<pair<int, int>> q;
+        queue<pair<int,int>> q;
         int r = grid.size();
         int c = grid[0].size();
-        int fresh = 0;
+        int rotten =0;
+        int fresh=0;
 
-        // Push all initially rotten oranges
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (grid[i][j] == 2) {
-                    q.push({i, j});
-                } else if (grid[i][j] == 1) {
-                    fresh++;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(grid[i][j] == 2){
+                q.push({i,j});
+                rotten++;
                 }
+                if(grid[i][j]==1) fresh++;
             }
         }
 
-        if (fresh == 0) return 0; // No fresh oranges to rot
+        if(fresh == 0) return 0;
 
-        int time = -1;
+        vector<int> dx = {0,-1,0,1};
+        vector<int> dy = {1,0,-1,0};
+        int min =0;
+        while(!q.empty()){
+            int size = q.size();
 
-        while (!q.empty()) {
-            int n = q.size();
-            time++; // One level = one minute
-
-            while (n--) {
-                pair<int, int> v = q.front();
+            while(size--){
+                auto node = q.front();
                 q.pop();
-                int i = v.first;
-                int j = v.second;
 
-                // check up
-                if (i - 1 >= 0 && grid[i - 1][j] == 1) {
-                    grid[i - 1][j] = 2;
-                    q.push({i - 1, j});
-                    fresh--;
-                }
-                // check down
-                if (i + 1 < r && grid[i + 1][j] == 1) {
-                    grid[i + 1][j] = 2;
-                    q.push({i + 1, j});
-                    fresh--;
-                }
-                // check left
-                if (j - 1 >= 0 && grid[i][j - 1] == 1) {
-                    grid[i][j - 1] = 2;
-                    q.push({i, j - 1});
-                    fresh--;
-                }
-                // check right
-                if (j + 1 < c && grid[i][j + 1] == 1) {
-                    grid[i][j + 1] = 2;
-                    q.push({i, j + 1});
-                    fresh--;
+                for(int i=0;i<4;i++){
+                    int newX = node.first + dx[i];
+                    int newY = node.second + dy[i];
+
+                    if(newX<r && newY<c && newX>=0 && newY>=0 && grid[newX][newY] == 1){
+                        q.push({newX,newY});
+                        grid[newX][newY] = 2;
+                        fresh--;
+                    }
                 }
             }
+            if(!q.empty()) min++;
         }
 
-        return (fresh == 0) ? time : -1;
+        if(fresh !=0) return -1;
+
+        return min;
     }
 };
