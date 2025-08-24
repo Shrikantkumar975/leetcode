942970811a1689ca31737310b58c4e9c00b84995
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2025-08-24 16:30:34 +0530

    Time: 8 ms (92.31%), Space: 34.6 MB (87.11%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,41 +1,43 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& grid) {
-         queue<pair<int, int>> q;
-        int r = grid.size();
-        int c = grid[0].size();
+         queue<pair<int,int>> q;
 
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (grid[i][j] == 0) {
-                    q.push({i, j});
-                } else if (grid[i][j] == 1) {
-                    grid[i][j]=-1;
+         int r = grid.size();
+         int c = grid[0].size();
+
+         for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(grid[i][j] == 0){
+                    q.push({i,j});
+                }
+                else if(grid[i][j] == 1){
+                    grid[i][j] = -1;
                 }
             }
-        }
+         }
+            vector<int> dx = {0,-1,0,1};
+            vector<int> dy = {1,0,-1,0};
 
-        vector<int> dx = {0,0,1,-1};
-        vector<int> dy = {1,-1,0,0};
-        // int time = 0;
-
-        while(q.size()>0){
+         while(!q.empty()){
             int size = q.size();
+
             while(size--){
-                pair<int,int> node = q.front();
+                auto node = q.front();
                 q.pop();
+                
                 for(int i=0;i<4;i++){
-                    int x = node.first + dx[i]; 
+                    int x = node.first + dx[i];
                     int y = node.second + dy[i];
 
-                    if(x>=0 && y>=0 && x<r && y<c && grid[x][y]==-1){
-                        grid[x][y] =  grid[node.first][node.second] + 1;
+                    if(x<r && y<c && x>=0 && y>=0 && grid[x][y]==-1){
+                        grid[x][y] = grid[node.first][node.second] + 1;
                         q.push({x,y});
-                    } 
+                    }
                 }
             }
-        }
+         }
 
-        return grid;
+         return grid;
     }
 };
