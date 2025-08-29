586d89810ab5303fdee27426a095e9192e3b6aed
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2025-08-29 17:10:06 +0530

    Time: 0 ms (100%), Space: 8.4 MB (48.89%) - LeetHub

---
+++ 3021-alice-and-bob-playing-flower-game/3021-alice-and-bob-playing-flower-game.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    long long flowerGame(int n, int m) {
+        long long count =0;
+        
+
+        count = (long long)n * m/2;
+
+        return count;
+    }
+};
