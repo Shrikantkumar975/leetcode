1f5bc442c0e5bc6403c0d286eb7019ddb2cb771f
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2025-09-02 23:34:54 +0530

    Time: 0 ms (100%), Space: 32.5 MB (88.89%) - LeetHub

--- 3025-find-the-number-of-ways-to-place-people-i/3025-find-the-number-of-ways-to-place-people-i.cpp
+++ 3025-find-the-number-of-ways-to-place-people-i/3025-find-the-number-of-ways-to-place-people-i.cpp
@@ -1,36 +1,68 @@
 class Solution {
 public:
     int numberOfPairs(vector<vector<int>>& point) {
+        // int n = point.size();
+        // int result =0;
+
+        // for(int i=0;i<n;i++){
+        //     int x1 = point[i][0];
+        //     int y1 = point[i][1];
+
+        //     for(int j=0;j<n;j++){
+        //         if(j==i) continue;
+
+        //         int x2 = point[j][0];
+        //         int y2 = point[j][1];
+                
+
+        //         if(x1<=x2 && y1>=y2){
+        //             int h = 0;  
+        //             for(int k=0;k<n;k++){
+        //                 if(k==i || k==j) continue;
+
+        //                 int x3 = point[k][0];
+        //                 int y3 = point[k][1];
+
+        //                 if(x3>=x1 && x3<=x2 && y3<=y1 && y3>=y2){
+        //                     h=1;
+        //                     break;
+        //                 }
+        //             }
+        //             if(h!=1){
+        //                 result++;
+        //             }
+        //         }
+
+        //     }
+        // }
+
+        // return result;
+
+
+
         int n = point.size();
         int result =0;
+        sort(point.begin(), point.end(), [](const vector<int>& a, const vector<int>& b) {
+        if (a[0] == b[0]) return a[1] > b[1]; // y in descending
+        return a[0] < b[0];                   // x in ascending
+    });
 
-        for(int i=0;i<n;i++){
+
+        for(int i=0;i<n-1;i++){
             int x1 = point[i][0];
             int y1 = point[i][1];
+            int maxY = INT_MIN;
 
-            for(int j=0;j<n;j++){
-                if(j==i) continue;
+            for(int j=i+1;j<n;j++){
 
                 int x2 = point[j][0];
                 int y2 = point[j][1];
-                
 
-                if(x1<=x2 && y1>=y2){
-                    int h = 0;  
-                    for(int k=0;k<n;k++){
-                        if(k==i || k==j) continue;
+                if(y2>y1) continue;
 
-                        int x3 = point[k][0];
-                        int y3 = point[k][1];
-
-                        if(x3>=x1 && x3<=x2 && y3<=y1 && y3>=y2){
-                            h=1;
-                            break;
-                        }
-                    }
-                    if(h!=1){
-                        result++;
-                    }
+                if(y2>maxY){
+                    result++;
+                    maxY=y2;
                 }
 
             }
