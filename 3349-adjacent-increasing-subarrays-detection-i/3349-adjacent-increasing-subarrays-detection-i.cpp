01ea87c0e4740b0b4027ff25a34df48a8e1da873
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2025-10-16 00:11:30 +0530

    Time: 19 ms (21.36%), Space: 40.5 MB (54.55%) - LeetHub

--- 3349-adjacent-increasing-subarrays-detection-i/3349-adjacent-increasing-subarrays-detection-i.cpp
+++ 3349-adjacent-increasing-subarrays-detection-i/3349-adjacent-increasing-subarrays-detection-i.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
+    int isIncreasing(vector<int> &nums,int start,int end){
+        for(int i=start;i<end;i++){
+            if(nums[i+1] <= nums[i]) return false;
+        }
+        return true;
+    }
+
     bool hasIncreasingSubarrays(vector<int>& nums, int k) {
-        int knew = k-1;
-        if(knew == 0)return true;
-        for(int j=k+1;j<nums.size();j++){
-            if(nums[j] > nums[j-1] && nums[j-k] > nums[j-k-1])knew--;
-            else knew = k - 1;
-            if(knew == 0)return true;
+        int n = nums.size();
+
+        if(2*k > nums.size()) return false;
+
+        for(int start = 0;start+2*k <= n;start++){
+            int first = isIncreasing(nums,start,start+k-1);
+            int second = isIncreasing(nums,start+k,start+2*k-1);
+
+            if(first == 1 && second == 1) return true;
         }
         return false;
     }
