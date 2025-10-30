bf1eeed7033294b071ff3e002d19b70fab06343d
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2025-10-30 19:06:44 +0530

    Time: 846 ms (18.17%), Space: 11.7 MB (34.23%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -29,8 +29,10 @@ public:
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
+                if(board[i][j]==word[0]){
                 back(board,word,i,j,0,ans);
                 if(ans) break;
+                }
             }
         }
 
