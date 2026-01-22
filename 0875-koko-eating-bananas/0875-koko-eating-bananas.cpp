c501a1385a28d333b37d5e1dad452eecb31c068d
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2026-01-22 11:19:57 +0530

    Time: 11 ms (37.65%), Space: 23.1 MB (11.9%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-
-    long checkMid(vector<int> arr,int mid){
-        long count=0;
-
-        for(long ele: arr){
-            count+=ceil(ele*1.00/mid);
+    long long check(int mid,vector<int> &piles){
+        long long ans = 0;
+        for(int i=0;i<piles.size();i++){
+            ans+=ceil(piles[i]*1.00/mid);
         }
-
-        return count;
+        return ans;
     }
 
     int minEatingSpeed(vector<int>& piles, int h) {
-        long low =1;
-        long high =1e9;
-        long ans = -1;
+        int low = 1;
+        int high = 1e9;
+        int ans=-1;
 
         while(low<=high){
-            long mid = low + (high-low)/2;
-            long a = checkMid(piles,mid);
-
-            if(a<=h){
-                ans = mid;
+            int mid = (high + low)/2;
+            long long curr = check(mid,piles);
+            if(curr<=h){
+                ans=mid;
                 high = mid-1;
+            }else{
+                low=mid+1;
             }
-            else low = mid+1;
         }
         return ans;
     }
