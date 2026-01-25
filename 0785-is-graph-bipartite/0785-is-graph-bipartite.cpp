b7f02e6f035a14d77418190bdd52ad760804d8c3
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2026-01-26 02:55:32 +0530

    Time: 0 ms (100%), Space: 17.5 MB (35.83%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -3,7 +3,8 @@ public:
     bool dfs(vector<vector<int>> &graph,vector<int> &color,int u,int col){
         color[u] = col;
 
-        int newCol = col==1?2:1;
+        int newCol = 3-col;
+
         for(int &v: graph[u]){
             if(color[v] == col){
                 return false;
@@ -22,11 +23,35 @@ public:
     bool isBipartite(vector<vector<int>>& graph) {
         int V = graph.size();
         vector<int> color(graph.size(),0);
+        
 
         for(int i=0;i<V;i++){
+            int col = 1;
             if(color[i]==0){
-                if(!dfs(graph,color,i,1))
-                    return false;
+                queue<int> q;
+                q.push(i);
+                color[i] = col;
+                while(!q.empty()){
+                    int size = q.size();
+
+                    while(size--){
+                        int newCol = 3-col;
+                        int u = q.front();
+                        q.pop();
+                        
+                        for(int &v: graph[u]){
+                            if(color[v] == col){
+                                return false;
+                            }
+                            if(color[v]==0){
+                                q.push(v);
+                                color[v]=newCol;
+                            }
+                        }
+                    }
+
+                    col= 3-col;
+                }
             }
         }
 
