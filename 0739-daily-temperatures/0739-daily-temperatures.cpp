153a20cc5e8c82dd86825bd820d789d2f6ba2320
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2026-01-27 02:16:50 +0530

    Time: 19 ms (72.83%), Space: 112.1 MB (8.78%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,23 +1,49 @@
 class Solution {
 public:
+    vector<int> findNGE(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> nse(n);
+        stack<int> st;
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && arr[st.top()] <= arr[i])
+                st.pop();
+            nse[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+        return nse;
+    }
+
     vector<int> dailyTemperatures(vector<int>& temp) {
-        stack<pair<int,int>> s;
-        vector<int> res(temp.size());
+        int n=temp.size();
+        vector<int> nge = findNGE(temp);
 
-        for(int i=temp.size()-1;i>=0;i--){
+        vector<int> ans;
 
-            while(!s.empty() && s.top().first<=temp[i]){
-                s.pop();
-            }
-            if(!s.empty()){
-                res[i]=s.top().second-i;
-            }else{
-                res[i]=0;
-            }
-            s.push({temp[i],i});
+        for(int i=0;i<temp.size();i++){
+            if(nge[i]==n) ans.push_back(0);
+            else ans.push_back(nge[i]-i);
         }
 
-        //brute force
+        return ans;
+
+        // stack<pair<int, int>> s;
+        // vector<int> res(temp.size());
+
+        // for (int i = temp.size() - 1; i >= 0; i--) {
+
+        //     while (!s.empty() && s.top().first <= temp[i]) {
+        //         s.pop();
+        //     }
+        //     if (!s.empty()) {
+        //         res[i] = s.top().second - i;
+        //     } else {
+        //         res[i] = 0;
+        //     }
+        //     s.push({temp[i], i});
+        // }
+
+        // brute force
 
         // for(int i=0;i<temp.size()-1;i++){
         //     int span=0;
@@ -30,6 +56,6 @@ public:
         //     if(j != temp.size()) res[i] = j-i;
         //     else res[i] =0;
         // }
-        return res;
+        // return res;
     }
 };
