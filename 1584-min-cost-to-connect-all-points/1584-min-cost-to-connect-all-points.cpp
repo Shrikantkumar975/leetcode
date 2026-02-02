89555e7942166569f240c20403196240ad47a6bf
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2026-02-03 03:33:39 +0530

    Time: 1071 ms (10.84%), Space: 250.8 MB (8.96%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,42 +1,71 @@
 class Solution {
 public:
-    using P = pair<int,int>;
+//Krushkal Algorithms
+    vector<int> parent;
+    vector<int> rank;
+
+    int find(int u){
+        if(parent[u]==u) return u;
+
+        return parent[u]=find(parent[u]);
+    }
+
+    void Union(int u,int v){
+        int pu = find(u);
+        int pv = find(v);
+
+        if(pu==pv) return;
+
+        if(rank[pu]<rank[pv]){
+            parent[pu]=pv;
+        }else if(rank[pv]<rank[pu]){
+            parent[pv]=pu;
+        }else{
+            parent[pv]=pu;
+            rank[pu]++;
+        }
+    }
+
     int minCostConnectPoints(vector<vector<int>>& points) {
-        int V =points.size();
-        vector<vector<pair<int,int>>> adj(V);
-
-        for(int i=0;i<V-1;i++){
-            for(int j=i+1;j<V;j++){
-                int d = abs(points[i][0]-points[j][0]) + abs(points[i][1] - points[j][1]);
-                adj[i].push_back({j,d});
-                adj[j].push_back({i,d});
+        int n= points.size();
+        vector<vector<int>> adj;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                int dist = abs(points[i][0]-points[j][0]) + abs(points[i][1]-points[j][1]);
+                adj.push_back({i,j,dist});
             }
         }
 
-        priority_queue<P,vector<P>,greater<P>> pq;
-        vector<bool> visited(V,false);
+        auto comp = [&](vector<int> &v1,vector<int> &v2){
+            return v1[2]<v2[2];
+        };
 
-        pq.push({0,0});
-        int sum=0;
+        parent.resize(n);
+        rank.resize(n,0);
 
-        while(!pq.empty()){
-            auto [d,u] = pq.top();
-            pq.pop();
-            
-            if(visited[u]) continue;
-
-            visited[u]=true;
-
-            sum+=d;
-
-            for(auto [v,w] : adj[u]){
-                if(!visited[v]){
-                    pq.push({w,v});
-                }
-            }
-
+        for(int i=0;i<n;i++){
+            parent[i]=i;
         }
 
-        return sum;
+        sort(adj.begin(),adj.end(),comp);
+
+        int ans=0;
+        for(auto a: adj){
+            int u=a[0];
+            int v=a[1];
+            int w=a[2];
+
+            int p1=find(u);
+            int p2=find(v);
+
+            if(p1!=p2){
+                Union(u,v);
+                ans+=w;
+            }
+        }
+
+        return ans;
+
+
     }
 };
