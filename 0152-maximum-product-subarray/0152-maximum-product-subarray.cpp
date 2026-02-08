8bb214786c3b3f69533d219d8a5ef2e1f8a5035e
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2026-02-08 16:46:53 +0530

    Time: 0 ms (100%), Space: 18.4 MB (5.03%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        vector<int> pp(nums.size(),0);
-        vector<int> sp(nums.size(),0);
-        pp[0]=nums[0];
-        sp[nums.size()-1]=nums[nums.size()-1];
+        int n = nums.size();
+        vector<int> pp(n, 0);
+        vector<int> sp(n, 0);
 
-        // int prod=1;
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]==0) continue;
-            pp[i]= nums[i]*pp[i-1];
+        pp[0] = nums[0];
+        sp[n-1] = nums[n-1];
+
+        // prefix product
+        for(int i = 1; i < n; i++){
+            if(nums[i] == 0)
+                pp[i] = 0;
+            else
+                pp[i] = (pp[i-1] == 0 ? nums[i] : nums[i] * pp[i-1]);
         }
 
-        for(int i=nums.size()-2;i>=0;i--){
-            if(nums[i]==0) continue;
-            sp[i]= nums[i]*sp[i+1];
+        // suffix product
+        for(int i = n - 2; i >= 0; i--){
+            if(nums[i] == 0)
+                sp[i] = 0;
+            else
+                sp[i] = (sp[i+1] == 0 ? nums[i] : nums[i] * sp[i+1]);
         }
 
-        int maxi=INT_MIN;
-
-        for(int i=0;i<nums.size();i++){
-            maxi = max(maxi,max(pp[i],sp[i]));
+        int maxi = INT_MIN;
+        for(int i = 0; i < n; i++){
+            maxi = max(maxi, max(pp[i], sp[i]));
         }
 
         return maxi;
