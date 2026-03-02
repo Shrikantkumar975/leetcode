57c2b57d1a3c4d90abb2332d0c84e32d69305b17
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2026-03-02 19:42:24 +0530

    Time: 4 ms (47.18%), Space: 19.1 MB (15.37%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,46 +10,31 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoList(ListNode* l1,ListNode* l2){
-        ListNode* dummy = new ListNode(0);
-        ListNode* temp = dummy;
-        while(l1 && l2){
-            if(l1->val <= l2->val){
-                temp->next=l1;
-                temp = temp->next;
-                l1=l1->next;
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<int,vector<int>,greater<int>> pq;
 
-            }
-            else{
-                temp->next=l2;
-                temp = temp->next;
-                l2=l2->next;
+        for(int i=0;i<lists.size();i++){
+            ListNode* temp = lists[i];
+
+            while(temp){
+                pq.push(temp->val);
+                temp=temp->next;
             }
         }
 
-        while(l1){
-            temp->next=l1;
-            temp=temp->next;
-            l1=l1->next;
-        }
-        
-        while(l2){
-            temp->next=l2;
-            temp=temp->next;
-            l2=l2->next;
-        }
+        ListNode* dummy=new ListNode(0);
+        ListNode* temp=dummy;
 
-        // temp->next=nullptr;
+        while(!pq.empty()){
+            int curr = pq.top();
+            pq.pop();
+
+            ListNode* newNode = new ListNode(curr);
+            temp->next = newNode;
+            temp=temp->next;
+        }
 
         return dummy->next;
-    }
 
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        ListNode* head = nullptr;
-        for(int i=0;i<lists.size();i++){
-            head = mergeTwoList(head,lists[i]);
-        }
-
-        return head;
     }
 };
