381580d82022afc9f3b0760097518cfa0cffef1f
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2026-03-04 02:58:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -1,27 +1,43 @@
 class Solution {
 public:
-    void helper(int start, const vector<int>& cookies, vector<int>& temp, int k, int& ans) {
-        int n = cookies.size();
-        if (start == n) {
-            int maxi = INT_MIN;
-            for (int i = 0; i < k; i++) {
-                maxi = max(maxi, temp[i]);
-            }
-            ans = min(ans, maxi);
+    int ans = INT_MAX;
+
+    void backtrack(int idx,
+                   vector<int>& cookies,
+                   vector<int>& children,
+                   int k) {
+
+        if(idx == cookies.size()) {
+            ans = min(ans, *max_element(children.begin(), children.end()));
             return;
         }
 
-        for (int i = 0; i < k; i++) {
-            temp[i] += cookies[start]; // take
-            helper(start + 1, cookies, temp, k, ans);
-            temp[i] -= cookies[start]; // back-track
+        int cookie = cookies[idx];
+
+        for(int i = 0; i < k; i++) {
+
+            // Prune
+            if(children[i] + cookie >= ans) continue;
+
+            children[i] += cookie;
+
+            backtrack(idx + 1, cookies, children, k);
+
+            children[i] -= cookie;
+
+            // Symmetry break
+            if(children[i] == 0) break;
         }
     }
 
     int distributeCookies(vector<int>& cookies, int k) {
-        int ans = INT_MAX;
-        vector<int> temp(k, 0);
-        helper(0, cookies, temp, k, ans);
+
+        sort(cookies.rbegin(), cookies.rend());
+
+        vector<int> children(k, 0);
+
+        backtrack(0, cookies, children, k);
+
         return ans;
     }
 };
