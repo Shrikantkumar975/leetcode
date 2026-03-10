bcefc3741fe2fb485b295170c48afd92390e5d8c
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2026-03-10 16:43:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -11,20 +11,38 @@
 class Solution {
 public:
     void reorderList(ListNode* head) {
-        ListNode* temp = head;
-        while(temp!=nullptr && temp->next != nullptr){
-            ListNode* last = temp;
-            ListNode* prev = nullptr;
-            while(last->next){
-                prev=last;
-                last=last->next;
-            }
-            
-            prev->next =nullptr;
-            ListNode* currNext = temp->next;
-            temp->next = last;
-            last->next=currNext;
-            temp=currNext;
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        //we find the middle of the LL
+        while(fast && fast->next){
+            slow = slow->next;
+            fast=fast->next->next;
         }
+
+        //we reverse the last part from middle of the LL
+        ListNode* curr= slow;
+        ListNode* nextNext = nullptr;
+        ListNode* prev = nullptr;
+
+        while(curr){
+            nextNext = curr->next;
+            curr->next =prev;
+            prev = curr;
+            curr=nextNext;
+        }
+
+        ListNode* temp = head;
+        ListNode* temp2 = prev;
+
+        while(temp->next && temp2->next){
+            ListNode* nextT2 = temp2->next;
+            ListNode* nextT1 = temp->next;
+            temp->next = temp2;
+            temp = nextT1;
+            temp2->next = nextT1;
+            temp2 = nextT2;
+        }
+
     }
 };
