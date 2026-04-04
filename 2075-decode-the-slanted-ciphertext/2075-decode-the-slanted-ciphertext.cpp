50de4cbac7ad8da2b0e3601e6fc625f2fd6bb5d1
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2026-04-04 23:24:12 +0530

    Time: 32 ms (56.78%), Space: 43.7 MB (49.15%) - LeetHub

--- 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.cpp
+++ 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.cpp
@@ -1,38 +1,43 @@
 class Solution {
 public:
-    string decodeCiphertext(string str, int rows) {
-        if(str.size()<=1) return str;
+    string decodeCiphertext(string str, int row) {
+        if(str.size()<=1 || row<=1) return str;
 
         int col = str.size()/row;
 
         vector<vector<char>> v(row,vector<char>(col));
 
-        int i=0;
+        int k=0;
 
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
-                v[i][j] = str[i++];
+                char ch = str[k];
+                v[i][j]=ch;
+                k++;
             }
         }
-
         string ans;
 
-        for(int i=0;i<row;i++){
+        for(int i=0;i<col;i++){
             int j=0;
             int k=i;
 
-            while(j<col && k<row){
+            while(j<row && k<col){
                 ans+=v[j][k];
                 j+=1;
                 k+=1;
             }
         }
 
-        trim(ans);
+
+        int i=0;
+        int j=ans.size()-1;
+
+        while(j >= 0 && ans[j] == ' ') j--;
+
+        return ans.substr(i,j-i+1);
 
         return ans;
-
-
 
     }
 };
