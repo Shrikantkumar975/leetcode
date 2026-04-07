dc729f2acc10732a198aad29843686c2d09355d4
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2026-04-07 16:52:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2069-walking-robot-simulation-ii/2069-walking-robot-simulation-ii.cpp
+++ 2069-walking-robot-simulation-ii/2069-walking-robot-simulation-ii.cpp
@@ -1,87 +1,53 @@
 class Robot {
 public:
-    int n, m;
-
-    int x = 0, y = 0, dir = 1;
-    vector<int> dx = {0, 1, 0, -1};
-    vector<int> dy = {1, 0, -1, 0};
+    int idx = 0;
+    bool moved = false;
+    vector<vector<int>> pos; //store all perimter/boundary cell positions
+    //O(width + height)
+    //S.C : (width + height)
     Robot(int width, int height) {
-        n = width;
-        m = height;
-    }
+        //Bottom Row (left to right) -> East(0);
+        for (int x = 0; x < width; x++) {
+            pos.push_back({x, 0, 0});
+        }
 
+        //Right Col (bottom to top) -> North(1);
+        for (int y = 1; y < height; y++) {
+            pos.push_back({width - 1, y, 1});
+        }
+
+        // Top row (right to left) -> West(2)
+        for (int x = width - 2; x >= 0; x--) {
+            pos.push_back({x, height - 1, 2});
+        }
+
+        // Left column (top to bottom) -> South(3)
+        for (int y = height - 2; y > 0; y--) {
+            pos.push_back({0, y, 3});
+        }
+
+        //pos = {{0, 0, 0}, ....}
+        pos[0][2] = 3; //Make it South for handkling corner case when it comes to (0,0) after moving
+
+    }
+    
     void step(int num) {
-        int cycle = 2 * (n + m - 2);
-        num %= cycle;
-
-        if (num == 0) {
-            if (x == 0 && y == 0)
-                dir = 2;
-            return;
-        }
-        while (num > 0) {
-            if (dir == 0) {
-                int curr = min(num, m - y - 1);
-                if (curr == 0) {
-                    dir = 3;
-                    continue;
-                }
-                y += curr;
-                num -= curr;
-                if (num > 0 && y == m - 1)
-                    dir = 3;
-            } else if (dir == 1) {
-                int curr = min(num, n - x - 1);
-                if (curr == 0) {
-                    dir = 0;
-                    continue;
-                }
-                x += curr;
-                num -= curr;
-                if (num > 0 && x == n - 1)
-                    dir = 0;
-            } else if (dir == 2) {
-                int curr = min(num, y);
-                if (curr == 0) {
-                    dir = 1;
-                    continue;
-                }
-                y -= curr;
-                num -= curr;
-                if (num > 0 && y == 0)
-                    dir = 1;
-            } else {
-                int curr = min(num, x);
-                if (curr == 0) {
-                    dir = 2;
-                    continue;
-                }
-                x -= curr;
-                num -= curr;
-                if (num > 0 && x == 0)
-                    dir = 2;
-            }
-        }
+        moved = true;
+        idx = (idx + num) % pos.size();
     }
-
-    vector<int> getPos() { return {x, y}; }
-
+    
+    vector<int> getPos() {
+        return {pos[idx][0], pos[idx][1]};
+    }
+    
     string getDir() {
-        if (dir == 0)
-            return "North";
-        else if (dir == 1)
+        if(!moved)
             return "East";
-        else if (dir == 2)
-            return "South";
-        else
-            return "West";
+        
+        int d = pos[idx][2];
+        if(d == 0) return "East";
+        else if(d == 1) return "North";
+        else if(d == 2) return "West";
+        return "South";
     }
 };
-
-/**
- * Your Robot object will be instantiated and called as such:
- * Robot* obj = new Robot(width, height);
- * obj->step(num);
- * vector<int> param_2 = obj->getPos();
- * string param_3 = obj->getDir();
- */
