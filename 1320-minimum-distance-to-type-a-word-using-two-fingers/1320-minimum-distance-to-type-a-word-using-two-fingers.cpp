762038d51abcf44ae0a179b45baea479147b135a
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2026-04-13 05:16:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.cpp
+++ 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
-    int cal(int a, int b) {
-        int ax = a / 6, ay = a % 6, bx = b / 6, by = b % 6;
-        return abs(a / 6 - b / 6) + abs(a % 6 - b % 6);
+    int mini = INT_MAX;
+    pair<int, int> coord(char ch) {
+        int c = ch - 'A';
+        return {c / 6, c % 6};
+    }
+    void solve(string word, int i, int sum, char f1, char f2) {
+        if (i == word.size()) {
+            mini = min(mini, sum);
+            return;
+        }
+        auto [a, b] = coord(word[i]);
+        auto [x1, y1] = coord(f1);
+        auto [x2, y2] = coord(f2);
+    
+        int newSum = abs(x1-a)+abs(y1-b);
+        solve(word,i+1,sum+newSum,word[i],f2);
+
+        if (f2 != '0')
+            sum += abs(x2 - a) + abs(y2 - b);
+        solve(word, i + 1, sum, f1, word[i]);
     }
 
-    int minimumDistance(string s) {
-        int n = s.size(), dp[300][26][26];
+    int minimumDistance(string word) {
 
-        for (int i = 0; i < n; i++) {
-            int t = s[i] - 'A';
-            for (int j = 0; j < 26; j++) {
-                for (int k = 0; k < 26; k++) {
-                    dp[i + 1][j][k] = 1e6;
-                }
-            }
-            for (int j = 0; j < 26; j++) {
-                for (int k = 0; k < 26; k++) {
-                    dp[i + 1][j][t] = min(dp[i + 1][j][t], dp[i][j][k] + cal(k, t));
-                    dp[i + 1][t][k] = min(dp[i + 1][t][k], dp[i][j][k] + cal(j, t));
-                }
-            }
-        }
+        solve(word, 1, 0, word[0], '0');
 
-        int ans = 1e6;
-        for (int j = 0; j < 26; j++) {
-            for (int k = 0; k < 26; k++) {
-                ans = min(ans, dp[n][j][k]);
-            }
-        }
-        return ans;
+        return mini;
     }
 };
