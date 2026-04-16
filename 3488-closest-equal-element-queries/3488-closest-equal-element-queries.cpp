36f3a2870a3df1a2964622fc6fb1c7e3e62b1624
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2026-04-17 03:48:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3488-closest-equal-element-queries/3488-closest-equal-element-queries.cpp
+++ 3488-closest-equal-element-queries/3488-closest-equal-element-queries.cpp
@@ -2,60 +2,46 @@ class Solution {
 public:
     vector<int> solveQueries(vector<int>& nums, vector<int>& queries) {
         int n = nums.size();
+        
+        unordered_map<int, vector<int>> mp;
 
-        vector<int> arr = nums;
-        for (int x : nums) arr.push_back(x);
-
-        int m = arr.size();
-
-        vector<int> left(m, INT_MAX);
-        vector<int> right(m, INT_MAX);
-
-        unordered_map<int, int> mpp;
-
-        // left pass
-        for (int i = 0; i < m; i++) {
-            if (mpp.find(arr[i]) != mpp.end()) {
-                left[i] = i - mpp[arr[i]];
-            }
-            mpp[arr[i]] = i;
+        for(int i = 0; i < n; i++) {
+            mp[nums[i]].push_back(i);
         }
 
-        mpp.clear();
+        vector<int> result;
 
-        // right pass
-        for (int i = m - 1; i >= 0; i--) {
-            if (mpp.find(arr[i]) != mpp.end()) {
-                right[i] = mpp[arr[i]] - i;
+        for(int qi : queries) { //O(Q)
+            int element = nums[qi];
+            vector<int>& vec = mp[element];
+
+            int sz = vec.size();
+
+            //no more occurence of this element
+            if(sz == 1) {
+                result.push_back(-1);
+                continue;
             }
-            mpp[arr[i]] = i;
-        }
 
-        vector<int> ans;
-
-        for (int q : queries) {
+            int pos = lower_bound(begin(vec), end(vec), qi) - begin(vec); //log(n)
             int res = INT_MAX;
 
-            // check all possible relevant positions
-            int idx1 = q;
-            int idx2 = q + n;
+            //Right Neighbour - pos+1
+            int right = vec[(pos+1) % sz];
+            int d = abs(qi - right);//straight forward distance
+            int circularDist = n-d;
+            res = min({res, d, circularDist});
 
-            // try both copies
-            for (int idx : {idx1, idx2}) {
-                if (left[idx] != INT_MAX) {
-                    int d = left[idx];
-                    res = min(res, min(d, n - d));
-                }
-                if (right[idx] != INT_MAX) {
-                    int d = right[idx];
-                    res = min(res, min(d, n - d));
-                }
-            }
 
-            if (res == 0) ans.push_back(-1);
-            else ans.push_back(res);
+            //Left Neighbour - pos-1
+            int left = vec[(pos-1+sz) % sz];
+            d = abs(qi - left);//straight forward distance
+            circularDist = n-d;
+            res = min({res, d, circularDist});
+
+            result.push_back(res);
         }
 
-        return ans;
+        return result;
     }
 };
