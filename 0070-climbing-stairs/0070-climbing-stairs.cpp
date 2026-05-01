7fd84517db2076139d3012a6bb8718c106f354aa
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2026-05-02 04:48:49 +0530

    Time: 0 ms (100%), Space: 7.7 MB (98.42%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
+    int ans[46];
+    int ways(int curr,int tar){
+        if(curr>tar) return 0;
+
+        if(ans[curr]!=-1) return ans[curr];
+        
+        if(curr==tar) return 1;
+
+
+        return ans[curr] = ways(curr+1,tar) + ways(curr+2,tar);
+    }
+
     int climbStairs(int n) {
-        if(n==0 || n==1) return 1;
-        vector<int> arr(n+1);
+        memset(ans,-1,sizeof(ans));
+        ans[n]=ways(0,n);
 
-        arr[0] =1;
-        arr[1] =1;
-
-        for(int i=2;i<=n;i++){
-            arr[i] = arr[i-1]+arr[i-2];
-        }
-
-        return arr[n];
+        return ans[n];
     }
 };
