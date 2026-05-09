ad2385a95226040c5ec3ec0a1f718d169170211a
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2026-05-10 03:55:50 +0530

    Time: 16 ms (32.69%), Space: 56.7 MB (89.03%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.cpp
+++ 1861-rotating-the-box/1861-rotating-the-box.cpp
@@ -3,7 +3,34 @@ public:
     vector<vector<char>> rotateTheBox(vector<vector<char>>& boxGrid) {
         int n = boxGrid.size();
         int m = boxGrid[0].size();
-        vector<vector<char>> newBox(m,vector<char>(n,'0'));
+
+
+
+        for(int k=0;k<n;k++){
+            int i=m-1;
+            int j=m-1;
+
+            while(j>=-1){
+                if(j==-1 || boxGrid[k][j]=='*'){
+                    while(i>j){
+                        boxGrid[k][i]='.';
+                        i--;
+                    }
+                    j--;
+                    i=j;
+                }else if(boxGrid[k][j]=='#'){
+                    boxGrid[k][i]='#';
+                    i--;
+                    j--;
+                }else{
+                    j--;
+                }
+            }
+
+
+        }
+
+        vector<vector<char>> newBox(m,vector<char>(n,'.'));
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
@@ -13,50 +40,6 @@ public:
 
         for(int i=0;i<m;i++){
             reverse(newBox[i].begin(),newBox[i].end());
-        }
-
-        for(int i=0;i<n;i++){
-            int count=0;
-            int curr = -1;
-            stack<pair<int,int>> st;
-            // st.push(-1);
-            for(int j=0;j<m;j++){
-                if(boxGrid[i][j]=='*'){
-                    st.push({curr,count});
-                    curr=j;
-                    count=0;
-                }
-                else if(boxGrid[i][j] == '#') count++;
-            }
-
-            st.push({curr,count});
-            
-            int col = n-i-1;
-            int row = m-1;
-
-            while(!st.empty()){
-                auto [till,cnt] = st.top();
-                st.pop();
-
-                while(row > -1){
-                    if(row==till){
-                        newBox[row][col]='*';
-                        row--;
-                    }
-                    if(cnt==0){
-                        while(row>till && row>=-1){
-                            newBox[row][col]='.';
-                            row--;
-                        }
-                        if(row!=-1)
-                            newBox[row][col]='*';
-                        row=till-1;
-                        break;
-                    }
-                    newBox[row--][col] = '#';
-                    cnt--;
-                }
-            }
         }
 
         return newBox;
