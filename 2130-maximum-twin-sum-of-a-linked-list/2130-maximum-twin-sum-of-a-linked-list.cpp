fede1e6ef45ad6c8c7ae749af95d79ac5b77e883
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2026-06-15 01:24:35 +0530

    Time: 7 ms (30.69%), Space: 138.5 MB (10.47%) - LeetHub

---
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int pairSum(ListNode* head) {
+        ListNode* temp = head;
+        vector<int>arr;
+        while(temp != nullptr){
+            arr.push_back(temp->val);
+            temp = temp->next;
+        }
+        int i = 0 ;
+        int j = arr.size() -1;
+        int maxi = INT_MIN;
+        while(i < j){
+            int a = arr[i]+arr[j];
+            maxi = max(a , maxi);
+            i++;j--;
+        }
+        return maxi;
+    }
+};
