af2c49ec96d22385eb6a54c3855e7abb56d2dbbb
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2026-07-17 14:37:33 +0530

    Time: 0 ms (100%), Space: 22.1 MB (95.66%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -1,44 +1,50 @@
 class Solution {
 public:
     int m, n;
-    vector<vector<int>> directions = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+    vector<int> dx = {1,-1,0,0};
+    vector<int> dy = {0,0,1,-1};
+
+    void dfs(int i, int j, vector<vector<int>>& heights,
+             vector<vector<bool>>& vis) {
+        vis[i][j] = true;
+
+        for (int k=0;k<4;k++) {
+            int x =i+dx[k], y = j + dy[k];
+
+            if (x < 0 || x >= m || y < 0 || y >= n || vis[x][y] || heights[x][y] < heights[i][j])
+                continue;
+
+            dfs(x, y, heights, vis);
+        }
+    }
 
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         m = heights.size();
         n = heights[0].size();
 
-        vector<vector<bool>> pacific(m, vector<bool>(n, false));
-        vector<vector<bool>> atlantic(m, vector<bool>(n, false));
+        vector<vector<bool>> arr1(m, vector<bool>(n, false));
+        vector<vector<bool>> arr2(m, vector<bool>(n, false));
 
-        for (int j = 0; j < n; j++) dfs(0, j, heights, pacific);
-        for (int i = 0; i < m; i++) dfs(i, 0, heights, pacific);
+        for (int j = 0; j < n; j++)
+            dfs(0, j, heights, arr1);
+        for (int i = 0; i < m; i++)
+            dfs(i, 0, heights, arr1);
 
-        for (int j = 0; j < n; j++) dfs(m-1, j, heights, atlantic);
-        for (int i = 0; i < m; i++) dfs(i, n-1, heights, atlantic);
+        for (int j = 0; j < n; j++)
+            dfs(m - 1, j, heights, arr2);
+        for (int i = 0; i < m; i++)
+            dfs(i, n - 1, heights, arr2);
 
-        vector<vector<int>> result;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (pacific[i][j] && atlantic[i][j]) {
-                    result.push_back({i, j});
+        vector<vector<int>> ans;
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(arr1[i][j] && arr2[i][j]){
+                    ans.push_back({i,j});
                 }
             }
         }
 
-        return result;
-    }
-
-    void dfs(int i, int j, vector<vector<int>>& heights, vector<vector<bool>>& visited) {
-        visited[i][j] = true;
-        
-        for (auto& d : directions) {
-            int x = i + d[0], y = j + d[1];
-            
-            if (x < 0 || x >= m || y < 0 || y >= n) continue;
-            if (visited[x][y]) continue;
-            if (heights[x][y] < heights[i][j]) continue;
-            
-            dfs(x, y, heights, visited);
-        }
+        return ans;
     }
 };
