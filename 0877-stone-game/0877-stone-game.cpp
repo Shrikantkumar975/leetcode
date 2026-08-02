66494943a94455c6bed8258c6dfb6185ea354988
Shrikant Kumar <143180518+Shrikantkumar975@users.noreply.github.com>
2026-08-03 01:19:22 +0530

    Time: 0 ms (100%), Space: 10.4 MB (68.89%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int alice = 0;
+        int bob = 0;
+
+    int i=0;
+    int j=piles.size()-1;
+    int curr=1;
+        while(i<=j){
+            if(curr%2==1){
+                if(piles[i]>=piles[j]){
+                    alice+=piles[i];
+                    i++;
+                }else{
+                    alice+=piles[j];
+                    j--;
+                }
+            }else{
+                if(piles[i]>=piles[j]){
+                    bob+=piles[i];
+                    i++;
+                }else{
+                    bob+=piles[j];
+                    j--;
+                }
+            }
+        }
+    return alice>=bob;
     }
+
 };
